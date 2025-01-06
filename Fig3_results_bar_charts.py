6bfc6e810f342682d7a30219368d169adb938596
Vytautas Kučinskas <vytkuc@gmail.com>
2025-01-06 17:06:17 +0200

    Bar charts update

--- Fig3_results_bar_charts.py
+++ Fig3_results_bar_charts.py
@@ -24,7 +24,7 @@ cmap = plt.get_cmap('tab10')
 colors = [cmap(i) for i in range(4)]
 
 
-def plot_barchart(ax, data, title):
+def plot_barchart(ax, el, data, title):
     mean_values = [np.mean(metric) for metric in data]
     std_values = [np.std(metric) for metric in data]
 
@@ -33,13 +33,13 @@ def plot_barchart(ax, data, title):
     right_ylim = 1.0  # Maximum for right Y-axis (AUC)
 
     # Plot Acc, Sens, Spec on left Y-axis
-    bar_positions = np.arange(len(mean_values))
+    bar_positions = np.arange(len(mean_values)) + (4*el) + el
     capped_std_left = [
         min(std, (left_ylim - 100 * mean) / 100) if 100 * mean + 100 * std > left_ylim else std
         for mean, std in zip(mean_values[:3], std_values[:3])
     ]
     ax.bar(bar_positions[:3], [100 * mean for mean in mean_values[:3]], align='center', alpha=0.7,
-           color=colors[:3], ecolor='black', capsize=5, width=0.5, label='Acc, Sens, Spec')
+           color=colors[:3], ecolor='black', capsize=5, width=0.9, label='Acc, Sens, Spec')
     ax.errorbar(bar_positions[:3], [100 * mean for mean in mean_values[:3]],
                 yerr=[100 * std for std in capped_std_left], fmt='none', ecolor='black', capsize=5)
 
@@ -53,19 +53,21 @@ def plot_barchart(ax, data, title):
         for mean, std in zip(mean_values[3:], std_values[3:])
     ]
     ax2.bar(bar_positions[3:], mean_values[3:], align='center', alpha=0.7,
-            color=colors[3], ecolor='black', capsize=5, width=0.5, label='AUC')
+            color=colors[3], ecolor='black', capsize=5, width=0.9, label='AUC')
     ax2.errorbar(bar_positions[3:], mean_values[3:], yerr=capped_std_right, fmt='none', ecolor='black', capsize=5)
 
     ax2.set_ylabel("AUC")
     ax2.set_ylim(0, right_ylim)
 
-    ax.set_title(title)
-    ax.set_xticks(bar_positions)
-    ax.set_xticklabels(['Acc', 'Sens', 'Spec', 'AUC'])
+    group_center = (bar_positions[0] + bar_positions[-1]) / 2  # Center of the group
+    ax.text(group_center, 110.0, title, ha="center", va="top", fontsize=12)
+    
 
     # Remove only the top frame line, retain error bar caps
     ax.spines['top'].set_visible(False)
     ax2.spines['top'].set_visible(False)
+
+    return bar_positions.tolist()
 
 
 datasets = [5, 10, 20]
@@ -84,18 +86,29 @@ for number_of_features in datasets:
              6: 'OC-SVM', 7: 'FNN-A'}
 
     # Set up 2x4 subplot layout
-    fig, axs = plt.subplots(2, 4, figsize=(14, 8), constrained_layout=True)
+    fig, axs = plt.subplots(2, 1, figsize=(14, 8), constrained_layout=True)
+    
+    # DBS_outcome_prediction_accuracy(accs[el], sens[el], spes[el], aucs[el], names[el])
 
-    for i, el in enumerate([0, 1, 2, 3, 4, 5, 6, 7]):
-        DBS_outcome_prediction_accuracy(accs[el], sens[el], spes[el], aucs[el], names[el])
+    # Map each subplot index to its location in the 2x4 grid
+    all_bar_positions = []
+    for el in range(4):
+        all_bar_positions += plot_barchart(axs[0], el, [accs[el], sens[el], spes[el], aucs[el]], names[el])
+        plot_barchart(axs[1], el, [accs[el+4], sens[el+4], spes[el+4], aucs[el+4]], names[el+4])
 
-        # Map each subplot index to its location in the 2x4 grid
-        ax = axs[i // 4, i % 4]
-        plot_barchart(ax, [accs[el], sens[el], spes[el], aucs[el]], names[el])
+    axs[0].set_xticks(all_bar_positions)
+    axs[0].set_xticklabels(['Acc', 'Sens', 'Spec', 'AUC'] * 4)
+    axs[1].set_xticks(all_bar_positions)
+    axs[1].set_xticklabels(['Acc', 'Sens', 'Spec', 'AUC'] * 4)
 
+    # Create a legend mapping colors to metrics
+    legend_labels = [plt.Rectangle((0, 0), 1, 1, color=color) for color in colors]
+    axs[0].legend(legend_labels, ['Acc', 'Sens', 'Spec', 'AUC'], bbox_to_anchor=(0.5, -0.15), loc='upper center', ncol=4, frameon=False)
+    axs[1].legend(legend_labels, ['Acc', 'Sens', 'Spec', 'AUC'], bbox_to_anchor=(0.5, -0.15), loc='upper center', ncol=4, frameon=False)
+    
     plt.tight_layout()
 
     figpath = os.path.join("figures", "Fig3_Bar_charts_" + str(number_of_features) + "_features.png")
     fig.savefig(figpath, dpi=300, bbox_inches='tight', format='png')
-    plt.show()
+    # plt.show()
         
